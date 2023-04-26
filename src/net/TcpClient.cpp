bf89c38a41b8580e497e3ec84f5cbc0f584689bf
qin <>
2023-04-26 10:59:43 +0800

    update TcpClient and TcpServer, avoid to return nullptr

--- src/net/TcpClient.cpp
+++ src/net/TcpClient.cpp
@@ -7,80 +7,75 @@
 #include <iostream>
 
 namespace net {
-    std::shared_ptr<std::vector<unsigned char>>
-    TcpClient::sendAndReceive(const std::string &ip, const unsigned short &port,
-                              const std::vector<unsigned char> &bytes) {
-        auto ptr = std::make_shared<std::vector<unsigned char>>();
-        auto flag = false;
+std::shared_ptr<std::vector<unsigned char>>
+TcpClient::sendAndReceive(const std::string &ip, const unsigned short &port,
+                          const std::vector<unsigned char> &bytes) {
+    auto ptr = std::make_shared<std::vector<unsigned char>>();
 
-        /*
-         * create a socket obj
-         */
-        boost::asio::io_service ioService;
-        boost::asio::ip::tcp::socket socket(ioService);
-        boost::system::error_code errorCode;
+    /*
+     * create a socket obj
+     */
+    boost::asio::io_service ioService;
+    boost::asio::ip::tcp::socket socket(ioService);
+    boost::system::error_code errorCode;
 
-        for (auto i = 0; i < 1; i++) {
-            /*
-             * connect to a remote tcp endpoint
-             */
-            try {
-                auto ipAddress = boost::asio::ip::address::from_string(ip);
-                boost::asio::ip::tcp::endpoint endpoint(ipAddress, port);
-                socket.connect(endpoint);
-            } catch (std::exception &e) {
-                std::cerr << "connect to " << ip << ":" << port << " error, exception message: " << e.what() << std::endl;
-                break;
-            }
-
-            /*
-             * send
-             */
-            auto bytesBuf = boost::asio::buffer(bytes);
-            try {
-                auto len = socket.write_some(bytesBuf, errorCode);
-                std::cout << "write " << len << " bytes." << std::endl;
-            } catch (std::exception &e) {
-                std::cerr << "write message to socket error, exception message: " << e.what() << std::endl;
-                std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-                break;
-            }
-
-            /*
-             * block and receive
-             */
-            try {
-                unsigned char oneKB[1024];
-                while (true) {
-                    auto len = socket.read_some(boost::asio::buffer(oneKB), errorCode);
-                    if (len > 0) {
-                        for (auto i = 0; i < len; i++) {
-                            ptr->push_back(oneKB[i]);
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            } catch (std::exception &e) {
-                std::cerr << "read message from socket error, exception message: " << e.what() << std::endl;
-                std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-                break;
-            }
-
-            /*
-             * close socket
-             */
-            try {
-                socket.close();
-            } catch (std::exception &e) {
-                std::cerr << "close socket error, exception message: " << e.what() << std::endl;
-                std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-                break;
-            }
-
-            flag = true;
-        }
-
-        return flag ? ptr : nullptr;
+    /*
+     * connect to a remote tcp endpoint
+     */
+    try {
+        auto ipAddress = boost::asio::ip::address::from_string(ip);
+        boost::asio::ip::tcp::endpoint endpoint(ipAddress, port);
+        socket.connect(endpoint);
+    } catch (std::exception &e) {
+        std::cerr << "connect to " << ip << ":" << port
+                  << " error, exception message: " << e.what() << std::endl;
     }
-}// namespace net
+
+    /*
+     * send
+     */
+    auto bytesBuf = boost::asio::buffer(bytes);
+    try {
+        auto len = socket.write_some(bytesBuf, errorCode);
+        std::cout << "write " << len << " bytes." << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "write message to socket error, exception message: "
+                  << e.what() << std::endl;
+        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
+    }
+
+    /*
+     * block and receive
+     */
+    try {
+        unsigned char oneKB[1024];
+        while (true) {
+            auto len = socket.read_some(boost::asio::buffer(oneKB), errorCode);
+            if (len > 0) {
+                for (auto i = 0; i < len; i++) {
+                    ptr->push_back(oneKB[i]);
+                }
+            } else {
+                break;
+            }
+        }
+    } catch (std::exception &e) {
+        std::cerr << "read message from socket error, exception message: "
+                  << e.what() << std::endl;
+        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
+    }
+
+    /*
+     * close socket
+     */
+    try {
+        socket.close();
+    } catch (std::exception &e) {
+        std::cerr << "close socket error, exception message: " << e.what()
+                  << std::endl;
+        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
+    }
+
+    return ptr;
+}
+} // namespace net
--- src/net/TcpServer.cpp
+++ src/net/TcpServer.cpp
@@ -3,58 +3,55 @@
 #include <iostream>
 
 namespace net {
-    std::shared_ptr<std::string> TcpServer::start(const unsigned short port, const std::string &rspToCli) {
-        auto res = std::make_shared<std::string>();
-        bool flag = false;
+std::shared_ptr<std::string> TcpServer::start(const unsigned short port,
+                                              const std::string &rspToCli) {
+    auto res = std::make_shared<std::string>();
 
-        /*
-         * create a socket obj
-         */
-        boost::asio::io_service ioService;
-        boost::asio::ip::tcp::socket socket(ioService);
-        boost::system::error_code errorCode;
+    /*
+     * create a socket obj
+     */
+    boost::asio::io_service ioService;
+    boost::asio::ip::tcp::socket socket(ioService);
+    boost::system::error_code errorCode;
 
-        for (auto i = 0; i < 1; i++) {
-            /*
-             * bind to a port, listen on it, and start to accept connections
-             */
-            try {
-                boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
-                boost::asio::ip::tcp::acceptor acceptor(ioService, endpoint);
-                acceptor.accept(socket, errorCode);
-            } catch (std::exception &e) {
-                std::cerr << "accept error, exception message: " << e.what() << std::endl;
-                std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-                break;
-            }
-
-            /*
-             * read
-             */
-            try {
-                boost::asio::streambuf streamBuf;
-                boost::asio::read_until(socket, streamBuf, '\n', errorCode);
-                res->append(boost::asio::buffer_cast<const char *>(streamBuf.data()));
-            } catch (std::exception &e) {
-                std::cerr << "read message from socket error, exception message: " << e.what() << std::endl;
-                std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-                break;
-            }
-
-            /*
-             * write
-             */
-            try {
-                boost::asio::write(socket, boost::asio::buffer(rspToCli), errorCode);
-            } catch (std::exception &e) {
-                std::cerr << "write message from socket error, exception message: " << e.what() << std::endl;
-                std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-                break;
-            }
-
-            flag = true;
-        }
-
-        return flag ? res : nullptr;
+    /*
+     * bind to a port, listen on it, and start to accept connections
+     */
+    try {
+        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(),
+                                                port);
+        boost::asio::ip::tcp::acceptor acceptor(ioService, endpoint);
+        acceptor.accept(socket, errorCode);
+    } catch (std::exception &e) {
+        std::cerr << "accept error, exception message: " << e.what()
+                  << std::endl;
+        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
     }
-}// namespace net
+
+    /*
+     * read
+     */
+    try {
+        boost::asio::streambuf streamBuf;
+        boost::asio::read_until(socket, streamBuf, '\n', errorCode);
+        res->append(boost::asio::buffer_cast<const char *>(streamBuf.data()));
+    } catch (std::exception &e) {
+        std::cerr << "read message from socket error, exception message: "
+                  << e.what() << std::endl;
+        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
+    }
+
+    /*
+     * write
+     */
+    try {
+        boost::asio::write(socket, boost::asio::buffer(rspToCli), errorCode);
+    } catch (std::exception &e) {
+        std::cerr << "write message from socket error, exception message: "
+                  << e.what() << std::endl;
+        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
+    }
+
+    return res;
+}
+} // namespace net
