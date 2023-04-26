e69be3024f50b93f2a3dc1f86cab3fb34b85efa4
qin <>
2023-04-26 10:25:47 +0800

    update UdpSender, get remote Udp response

--- src/net/UdpSender.cpp
+++ src/net/UdpSender.cpp
@@ -1,35 +1,22 @@
 #include "UdpSender.h"
 #include <boost/asio.hpp>
+#include <cstring>
 #include <iostream>
 
 namespace net {
-bool UdpSender::send(const std::string &msg, const std::uint16_t localPort,
-                     const std::string &remoteAddr,
-                     const std::uint16_t remotePort) {
-    boost::asio::io_service ioService;
-    boost::system::error_code errorCode;
-    try {
-        boost::asio::ip::udp::endpoint localEndpoint(boost::asio::ip::udp::v4(),
-                                                     localPort);
-        boost::asio::ip::udp::endpoint remoteEndpoint(
-            boost::asio::ip::address::from_string(remoteAddr, errorCode),
-            remotePort);
-        boost::asio::ip::udp::socket socket(ioService, localEndpoint);
-        auto len = socket.send_to(boost::asio::buffer(msg), remoteEndpoint);
-        std::cout << "send " << len << " bytes successfully" << std::endl;
-    } catch (std::exception &e) {
-        std::cerr << "send message to socket error, exception message: "
-                  << e.what() << std::endl;
-        std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-        return false;
-    }
-    return true;
+std::shared_ptr<std::vector<std::uint8_t>>
+UdpSender::send(const std::string &msg, const std::uint16_t localPort,
+                const std::string &remoteAddr, const std::uint16_t remotePort) {
+    std::vector<std::uint8_t> bytes(msg.size());
+    std::memcpy(bytes.data(), msg.data(), msg.size());
+    return send(bytes, localPort, remoteAddr, remotePort);
 }
 
-bool UdpSender::send(const std::vector<std::uint8_t> &bytes,
-                     const std::uint16_t localPort,
-                     const std::string &remoteAddr,
-                     const std::uint16_t remotePort) {
+std::shared_ptr<std::vector<std::uint8_t>>
+UdpSender::send(const std::vector<std::uint8_t> &bytes,
+                const std::uint16_t localPort, const std::string &remoteAddr,
+                const std::uint16_t remotePort) {
+    auto res = std::make_shared<std::vector<std::uint8_t>>();
     boost::asio::io_service ioService;
     boost::system::error_code errorCode;
     try {
@@ -42,12 +29,19 @@ bool UdpSender::send(const std::vector<std::uint8_t> &bytes,
         auto len = socket.send_to(
             boost::asio::buffer(bytes.data(), bytes.size()), remoteEndpoint);
         std::cout << "send " << len << " bytes successfully" << std::endl;
+
+        std::uint8_t twoKB[2048] = {'\0'};
+        auto len2 =
+            socket.receive_from(boost::asio::buffer(twoKB), remoteEndpoint);
+        for (auto i = 0; i < len2; ++i) {
+            res->push_back(twoKB[i]);
+        }
+
     } catch (std::exception &e) {
         std::cerr << "send message to socket error, exception message: "
                   << e.what() << std::endl;
         std::cerr << boost::system::system_error(errorCode).what() << std::endl;
-        return false;
     }
-    return true;
+    return res;
 }
 } // namespace net
--- src/net/UdpSender.h
+++ src/net/UdpSender.h
@@ -2,20 +2,20 @@
 #define __UDPSENDER_H__
 
 #include <cstdint>
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace net {
 class UdpSender {
   public:
-    static bool send(const std::string &msg, const std::uint16_t localPort,
-                     const std::string &remoteAddr,
-                     const std::uint16_t remotePort);
+    static std::shared_ptr<std::vector<std::uint8_t>>
+    send(const std::string &msg, const std::uint16_t localPort,
+         const std::string &remoteAddr, const std::uint16_t remotePort);
 
-    static bool send(const std::vector<std::uint8_t> &bytes,
-                     const std::uint16_t localPort,
-                     const std::string &remoteAddr,
-                     const std::uint16_t remotePort);
+    static std::shared_ptr<std::vector<std::uint8_t>>
+    send(const std::vector<std::uint8_t> &bytes, const std::uint16_t localPort,
+         const std::string &remoteAddr, const std::uint16_t remotePort);
 };
 } // namespace net
 
